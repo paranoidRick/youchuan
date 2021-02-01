44df3c42c692c2abe08f2115b78dc1d81689a4b8
rick <ghl_rick@163.com>
2021-02-01 20:50:12 +0800

    add publish

--- src/main/java/com/rick/community/controller/AuthorizeController.java
+++ src/main/java/com/rick/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,7 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
 
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
@@ -47,13 +49,15 @@ public class AuthorizeController {
         if (githubUser != null){
             // 登录成功
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getLogin());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             // 登录失败
--- src/main/java/com/rick/community/controller/IndexController.java
+++ src/main/java/com/rick/community/controller/IndexController.java
@@ -1,16 +1,51 @@
 package com.rick.community.controller;
 
 
+import com.rick.community.dto.QuestionDTO;
+import com.rick.community.mapper.QuestionMapper;
+import com.rick.community.mapper.UserMapper;
+import com.rick.community.model.Question;
+import com.rick.community.model.User;
+import com.rick.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
+
     @GetMapping("/")
-    public String hello(){
+    public String hello(HttpServletRequest request, Model model){
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length!=0)
+            for (Cookie cookie:cookies)
+            {
+                if (cookie.getName().equals("token"))
+                {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user!=null) {
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
